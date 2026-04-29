1a0539de485deedfd09a5cb95b8d3ec97dd9cb9a
CodingMasterLSW <spqjekdl1004@gmail.com>
2026-04-29 21:49:00 +0900

    feat(routing): mount CallPage at /call/:roomId

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryProvider } from "@/providers/QueryProvider";
 import { MainPage } from "@/pages/main/MainPage";
 import { MatchingPage } from "@/pages/matching/MatchingPage";
+import { CallPage } from "@/pages/call/CallPage";
 import { MyPagePage } from "@/pages/mypage/MyPagePage";
 import { UserExpressionsPage } from "@/pages/userExpressions/UserExpressionsPage";
 import { CallHistoryPage } from "@/pages/callHistory/CallHistoryPage";
@@ -13,6 +14,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/matching" element={<MatchingPage />} />
+          <Route path="/call/:roomId" element={<CallPage />} />
           <Route path="/mypage" element={<MyPagePage />} />
           <Route path="/expressions" element={<UserExpressionsPage />} />
           <Route path="/history" element={<CallHistoryPage />} />
